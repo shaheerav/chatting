dad961766aac3d3e71c42adc8320917f412249b3
Shaheera <vshaheera89@gmail.com>
2026-07-08 14:00:51 +0530

    make some changes

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -14,10 +14,10 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const publicDir = path.join(process.cwd(), "back-end", "dist");
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 
 app.use(clerkMiddleware());
 
@@ -25,24 +25,12 @@ app.get("/chat", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
-const publicDir = path.join(process.cwd(), "dist");
-
-if (fs.existsSync(publicDir)) {
-  app.use(express.static(publicDir));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(publicDir, "index.html"));
-  });
-}
-
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
   app.get("/{*any}", (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
-console.log("Serving from:", publicDir);
-console.log("Exists:", fs.existsSync(publicDir));
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on part:", PORT);
