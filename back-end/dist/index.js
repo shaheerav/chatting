7ad4162f8696a4af666e7b2fea48423147845d79
Shaheera <vshaheera89@gmail.com>
2026-07-15 13:54:23 +0530

    clerk data connected

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -5,9 +5,13 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 
+import { clerkMiddleware } from "@clerk/express";
+
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-import { clerkMiddleware } from "@clerk/express";
+import job from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 
